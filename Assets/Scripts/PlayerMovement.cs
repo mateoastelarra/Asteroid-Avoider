9f77675632ebd44e4aaa8b3956fb25246340a495
Mateo Astelarra <mateo@astelarra.com>
2023-06-29 12:21:40 -0300

    Added Rotation and movement limiter for close touches

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,8 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float forceMagnitud;
     [SerializeField] private float maxVelocity;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float movementLimiter;
     private Vector3 movementDirection;
     private Camera mainCamera;
     private Vector2 position;
@@ -23,7 +25,10 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         KeepPlayerOnScreen();
+
         ProcessInput();
+
+        RotateToFaceVelocity();
     }
 
     private void FixedUpdate() 
@@ -42,9 +47,16 @@ public class PlayerMovement : MonoBehaviour
             position = Touchscreen.current.primaryTouch.position.ReadValue();
             Vector3 worldPosition = mainCamera.ScreenToWorldPoint(new Vector3 (position.x, position.y, 0));
             
-            movementDirection = worldPosition - transform.position;
-            movementDirection.z = 0;
-            movementDirection.Normalize();
+            if ((worldPosition - transform.position).magnitude < 10f + movementLimiter)
+            {
+                movementDirection = Vector3.zero;
+            }
+            else
+            {
+                movementDirection = worldPosition - transform.position;
+                movementDirection.z = 0;
+                movementDirection.Normalize();
+            }
         }
         else
         {
@@ -68,5 +80,14 @@ public class PlayerMovement : MonoBehaviour
         }
 
         transform.position = newPosition; 
+    }
+
+    private void RotateToFaceVelocity()
+    {
+        if (RB.velocity == Vector3.zero){return;}
+
+        Quaternion targetRotation = Quaternion.LookRotation(RB.velocity, Vector3.back);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
