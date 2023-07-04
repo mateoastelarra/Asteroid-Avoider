0235ab0bdf4f561acd32efc2c048fe9d87df0a45
Mateo Astelarra <mateo@astelarra.com>
2023-07-04 18:00:40 -0300

    Ad manager

--- Assets/Scripts/AdManager.cs
+++ Assets/Scripts/AdManager.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Advertisements;
 
-public class AdManager : MonoBehaviour
+public class AdManager : MonoBehaviour, IUnityAdsInitializationListener, IUnityAdsLoadListener, IUnityAdsShowListener
 {
     public static AdManager instance;
 
@@ -16,7 +17,47 @@ public class AdManager : MonoBehaviour
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
+
+            Advertisement.Initialize(gameId, testMode,this);
         }   
     }
 
+    public void OnInitializationComplete()
+    {
+        Debug.Log("Unity Ads Initialization complete");
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        Debug.Log($"Unity Ad Initialization Failed {error} - {message}");
+    }
+
+    public void OnUnityAdsAdLoaded(string placementId)
+    {
+        Debug.Log($"Unity Ad loaded: {placementId}");
+    }
+
+    public void OnUnityAdsFailedToLoad(string placementId, UnityAdsLoadError error, string message)
+    {
+        Debug.Log($"Unity Ad Failed to Load: {error} - {message}");
+    }
+
+    public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUnityAdsShowStart(string placementId){ }
+
+    public void OnUnityAdsShowClick(string placementId){ }
+
+    public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
+    {
+        switch(showResult)
+        {
+            case ShowResult.Finished:
+                GameOverHandler.Continue();
+                break;
+        }
+    }
 }
